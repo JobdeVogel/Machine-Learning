b09a16d55ce0d493f5e9ba542c1b23a9465ce287
Job de Vogel <jobdvogel@gmail.com>
2022-02-21 11:14:04 +0100

    Added open3d view pointcloud

---
+++ viewPointCloud.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+import open3d as o3d
+
+filename = os.path.join('./data', '430.xyz')
+
+"""
+0-99: Houses
+100-199: Cars
+200-299: Fences
+300-399: Traffic Lights
+400-499: Trees
+"""
+
+data = pd.read_table(filename, skiprows=0, delim_whitespace=True, names=['x', 'y', 'z'])
+print((data['y'].max(0) - data['y'].min(0)) / (data['x'].max(0) - data['x'].min(0)))
+print(data['z'].max(0))
+print(data['z'].min(0))
+
+
+data = data.values
+
+open3d_pointcloud = o3d.geometry.PointCloud()
+open3d_pointcloud.points = o3d.utility.Vector3dVector(data)
+o3d.visualization.draw_geometries([open3d_pointcloud])

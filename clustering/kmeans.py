f1f5863e7ec5cfb1d3864ae150cf7275ba5b1304
Job de Vogel <jobdvogel@gmail.com>
2022-03-03 14:28:45 +0100

    Final Update

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -5,17 +5,19 @@ import time
 
 def main(K, dataframe):
     start = time.time()
-    #Find dimension of data and establish k
+
+    # Find dimension of data and establish k
     dataframe = pd.DataFrame.to_numpy(dataframe)
     dataset = dataframe
     dim = dataset.shape[1]
 
-    #Find first random centroid for k-means++ centroid allocation
+    # Find first random centroid for k-means++ centroid allocation
     init_list = list(range(0, 500))
     step_avg = []
     sel_int = [np.random.randint(0, 499)]
     ci = [dataset[sel_int[0]]]
-    #Use distance-based probability to select all centroids
+
+    # Use distance-based probability to select all centroids
     for i in range(K):
         step_avg.append([list(ci[0])])
         init_list.pop(sel_int[0])
@@ -24,20 +26,20 @@ def main(K, dataframe):
         sel_int = np.random.choice(init_list, 1, p=dist_list)
         ci = [dataset[sel_int[0]]]
 
-    #Initialize comparison lists and iterations
+    # Initialize comparison lists and iterations
     last_avg = np.ones([K, dim])
     comparison = last_avg == step_avg
     iteration = 0
     max_iterations = 50
 
-    #Main K-means loop
+    # Main K-means loop
     while comparison.all() != True and iteration != max_iterations:
         result_list = list(range(0, 500))
         cycle_list = np.ndarray.tolist(np.copy(step_avg))
         last_avg = np.copy(step_avg)
         squeeze_step = np.squeeze(step_avg)
 
-        #Assign points to their clusters
+        # Assign points to their clusters
         j = 0
         for i in dataset:
             val = np.argmin(ds.cdist([i], squeeze_step)[0])
@@ -45,14 +47,14 @@ def main(K, dataframe):
             result_list[j] = val
             j += 1
         
-        #Calculate the new averages
+        # Calculate the new averages
         l = 0
         for i in cycle_list:
             avg = [sum(x)/len(x) for x in zip(*i)]
             step_avg[l] = [avg]
             l += 1
         
-        #Update comparison and iteration
+        # Update comparison and iteration
         comparison = last_avg == step_avg
         iteration += 1
 
@@ -69,7 +71,4 @@ def main(K, dataframe):
 # data_file = 'csv_data.csv'
 # dataframe = pd.read_csv(data_file, skiprows=0)
 # results = main(5, dataframe)
-# print(results)    
-    
-
-
+# print(results)

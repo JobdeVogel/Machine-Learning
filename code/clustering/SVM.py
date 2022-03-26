9effb47cf841490ae895627975469c2c0a8d1452
jirrivdb <jirrivdb@gmail.com>
2022-03-26 21:34:28 +0100

    SVM 2.0

--- code/clustering/SVM.py
+++ code/clustering/SVM.py
@@ -1,167 +1,158 @@
+from asyncio import constants
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, balanced_accuracy_score
+from seaborn import heatmap
 
-# Add the labels to the proximity matrix dataframe
-def add_labels(df):
-    targets = np.zeros(len(df), dtype=np.int16)
+#Add labels to dataset
+def addLabels(data):
+    targets = np.zeros(len(data), dtype=np.int16)
 
-    for i in range(len(df)):
+    for i in range(len(data)):
         targets[i] = int(i // 100)
+    
+    data['labels'] = targets
 
-    df['labels'] = targets
+    return data
 
-    return df
+#Find the best hyperparameters using GridSearchCV
+def findParameters(param_dict, check):
+    results = []
+    x_train, x_test, y_train, y_test = train_test_split(data.drop('labels', axis=1), data['labels'], test_size=0.5)
+    iterate = param_dict[check]
 
-#Main Support vector prediction, returning the accuracy score
-def support_vector(data, test_size, grid, random_state):
-    x_train, x_test, y_train, y_test = splitData(data, test_size, random_state)
-    y_pred = grid.predict(x_test)
-    score = accuracy_score(y_test, y_pred)
-    return score
+    #Saving only the found parameters to use in a seperately built model
+    for i in iterate:
+        param_dict[check] = [i]
+        results.append(GridSearchCV(SVC(), param_dict, refit=True, verbose=1).fit(x_train, y_train).best_params_)
 
-#Using a learning curve to find the optimal kernel to use for the supplied dataset
-def learning_curve(data, kernel, cycles, random_state):
+    return results
+
+#Main function for svm classification
+def supportVector(data, param, test_size, to_print = False):
+    x_train, x_test, y_train, y_test = train_test_split(data.drop('labels', axis=1), data['labels'], test_size=test_size)
+    clf = SVC(**param)
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+
+    # Overall accuracy
+    oA = accuracy_score(y_test, y_pred)
+
+    # Mean per class accuracy
+    mcA =balanced_accuracy_score(y_test, y_pred)
+
+    # Confusion Matrix
+    cM = confusion_matrix(y_test, y_pred)
+
+    #Output confusion matrix and accuracy reading
+    if to_print:
+        print('Overall accuracy score: {}'.format(round(oA, 2)))
+        print('Mean per class accuracy: {}'.format(round(mcA, 2)))
+
+        # Plot confusion matrix with seaborn
+        plt.figure(figsize=(5,5))
+        plt.grid(False)
+        heatmap(cM, annot = True, cmap="Greens")
+        plt.xlabel('Predicted label')
+        plt.ylabel('Real label')
+        plt.title('Confusion Matrix (Test Size {}%)'.format(test_size * 100))
+        plt.show()
+
+    return oA
+
+#Establishing a learning curve over test/train sizes
+def learningCurve(data, param):
     train_sizes = []
     accuracies = []
-    
-    #Finding the best parameters for the current kernel
-    grid = findCV(data, kernel, random_state)
-    print(grid.best_estimator_)
 
-    #Limiting the split analyzed to 0-80% greatly reduces noise
-    for i in range(20, 100):
-        test_size = []
-        scores = []
-        
-        #Running the full script for cycles to normalize random fluctuations
-        for j in range(0, cycles):
-            test_size = i / 100
-            score = support_vector(data, test_size, grid, random_state)
-            scores.append(score)
-        
-        #Averaging the scores    
-        score = sum(scores) / len(scores)
+    #Running for all test/train splits
+    for i in range(1, 100):
+        test_size = i / 100
+        score = supportVector(data, param, test_size)
 
-        #Add the results to a list
         train_sizes.append(100 - (test_size * 100))
-        accuracies.append(score * 100)
+        accuracies.append(score)
 
     return train_sizes, accuracies
 
-
-#Splitting the data using train_test_split 
-def splitData(data, test_size, random_state):
-    x = data.drop('labels', axis=1)
-    y = data['labels']
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=random_state)
-    return x_train, x_test, y_train, y_test
-
-#Using GridsearchCV the parameters are optimized using a standard 50,50 
-def findCV(data, kernel, random_state):
-    #Using a 50/50 split on the data to find the best parameters instead of running it for every split, massively reducing computation 
-    x_train, x_test, y_train, y_test = splitData(data, 0.5, random_state)
-
-    #Specify parameters to check for C, Gamma and degree optimization
-    #Add a degree check only to polynomial
-    if kernel == 'poly':
-        input_params = {'C': [0.1,1, 10, 100], 'gamma': [1,0.1,0.01,0.001],'degree': [2, 3, 4, 5, 7, 9], 'kernel': [kernel]}
-    else:
-        input_params = {'C': [0.1,1, 10, 100], 'gamma': [1,0.1,0.01,0.001], 'kernel': [kernel]}
+#Main Loop
+def main(data, param_check):
+    data = addLabels(data)
+    constant = []
+    #Preselected empirical best hyperparameters for this dataset
+    preselected_param = [{
+        'kernel' : 'rbf', 
+        'C' : 10,
+        'gamma' : 'scale',
+        'degree' : 2
+        }]
     
-    #Return the grid for prediction
-    grid = GridSearchCV(SVC(random_state=random_state), input_params, refit=True, verbose=1)
-    grid.fit(x_train, y_train)
-    return grid
+    #The parameter to check and display in graph
+    check = 'kernel'
+    
+    #Possibility to check, keep constant and optimize parameters
+    if param_check == True:
+        #Full parameter dictionary
+        param_dict = {
+        'kernel' : ['linear', 'poly', 'rbf', 'sigmoid'],
+        'C' : [0.1, 1, 10, 100],
+        'gamma' : ['scale', 'auto'],
+        'degree' : [2, 3, 4, 5, 7, 9]
+        }
 
-#Check for most and least accurate data labeling
-def CheckLabelAcc(data):
-    data = add_labels(data)
-    kernel = 'rbf'
-    test_size = 0.5
-    random_state = None
-    names = ['building', 'car', 'fence', 'pole', 'tree']
-    # 000 - 099: building;
-    # 100 - 199: car;
-    # 200 - 299: fence;
-    # 300 - 399: pole;
-    # 400 - 499: tree.
-    tick_labels = []
-    scores = []
-    freq = []
-    width = 0.1
-    x = 0
-    for i in range(0,5):
-        print (i)
-        nameA = names[i]
-        for j in range(0, 5):
-            if j > i:
-                x += 0.2
-                nameB = names[j]
-                select_data = data.loc[data['labels'].isin([i, j])]
-                grid = findCV(select_data, kernel, random_state)
-                score = support_vector(select_data, test_size, grid, random_state)
-                print (score)
-                tick_labels.append(nameA + '/' + nameB)
-                scores.append(score)
-                freq.append(x)
+        #Providing a constant value will override the full param_dict
+        constant = [
+            #('kernel', 'rbf'),
+            #('C', 1),
+            #('gamma', 'scale'),
+            #('degree', 2)
+            ]
 
-    fig, ax = plt.subplots()
-    rects = ax.bar(freq, scores, width, color='#fcba03')
-    ax.set_ylim(0,1.1)
-    ax.set_ylabel('Accuracy')
-    ax.set_title('Specific Relation Accuracy')
-    ax.set_xticks(np.add(freq,width/16)) # set the position of the x ticks
-    ax.set_xticklabels(tick_labels)
+        #Loop to override      
+        for i in constant:
+            param_dict[i[0]] = [i[1]]
+        labels = param_dict[check]
 
+        #Finding the best params to use for the different values to check
+        params = findParameters(param_dict, check)
+        print(params)
+
+    #Select the preselected params by setting param_check to False 
+    elif param_check == False:
+        params = preselected_param
+        labels = [preselected_param[0]['kernel']]
+    
+    #Catching param_check != Boolean value
+    else:
+        print('Please provide a boolean value for param_check')
+
+    #For the provided param sets run the learning curve
     i = 0
-    for rect in rects:
-        height = rect.get_height()
-        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
-                scores[i],
-                ha='center', va='bottom')
-        i+=1
-
-    plt.show()
-
-#Main callable script loop
-def main(data):
-    data = add_labels(data)
-
-    #List of Kernels to try
-    kernels = ['linear', 'poly', 'rbf', 'sigmoid']
-
-    #Amount of cycles to average to reduce random fluctuations
-    cycles = 10
-
-    #Possibility to specify a seed to eliminate random chance (None or any int)
-    #It is advised to set cycles to 1 when specifying a random_state
-    random_state = None
-
-    #Run the script
-    for kernel in kernels:
-        train_sizes, accuracies = learning_curve(data, kernel, cycles, random_state)  
-        
-        #Add the result lists to a plot
-        plt.plot(train_sizes, accuracies, label="kernel = {}".format(kernel))
-        print('')
-
-    # Plot the result
+    for param in params:
+        train_sizes, accuracies = learningCurve(data, param)
+        plt.plot(train_sizes, accuracies, label=check + "= {}".format(labels[i]))
+        i += 1
+    
+    #Visualize the params to check
     plt.xlabel('Training size (%)')
     plt.ylabel('Overall Accuracy (%)')
     plt.title('Learning Curve')
+    plt.figtext(0.15, 0.05, 'Constant:' + str(constant))
     plt.grid()
     plt.legend()
     plt.show()
-    
 
+    #Running at 30% Test size for confusion matrix and accuracy reading
+    #Be warned this will always run for the preselected params
+    supportVector(data, preselected_param[0], test_size = 0.3, to_print=True)
 
+if __name__ == "__main__":
+    np.random.seed(2)
 
-
-data = pd.read_csv('./code/csv_data.csv')
-#result = main(data)
-CheckLabelAcc(data)
+    data = pd.read_csv('./code/csv_data.csv')
+    param_check = False
+    main(data, param_check)
